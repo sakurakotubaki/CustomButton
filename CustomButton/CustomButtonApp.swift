64721bf0c579bfa7eed01ea276c92687f531c890
Jboy422 <gitonly543@gmail.com>
2024-06-26 06:43:56 +0900

    Initial Commit

---
+++ CustomButton/CustomButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomButtonApp.swift
+//  CustomButton
+//
+//  Created by Jboy422 on 2024/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CustomButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
